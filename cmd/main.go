8e57f133fef31d399bb6a939f1b4624c6e44150d
Amir Mohammad Mohammadi <HP@LAPTOP-OPMTJQNC>
2025-11-25 14:26:12 +0330

    ADD MQTT client initialization and update dependencies for websocket and net packages

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"github.com/NiflheimDevs/dyslexics-clock/internal/mqtt"
+)
+
 func main() {
-	
+	// 1. Connect to MQTT
+	mqttClient, err := mqtt.New("tcp://localhost:1883", "backend-server")
+	if err != nil {
+		log.Fatal("MQTT connect error:", err)
+	}
+
+	// 2. Init services
+	//deviceService := device.New()
+	//alarmService := alarm.New()
+
+	// 3. Register MQTT subscribers
+	sub := mqtt.NewSubscriber(mqttClient /* alarmService, deviceService */)
+	sub.RegisterHandlers()
+
+	select {}
 }
