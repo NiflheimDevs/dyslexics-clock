6157a45b1725b6fdc295ac02fa4c014ac3698dd1
kiarash <kiarashs37@gmail.com>
2025-12-26 21:28:37 +0330

    fix env issue

--- internal/infra/message_broker/driver/driver.go
+++ internal/infra/message_broker/driver/driver.go
@@ -2,13 +2,16 @@ package driver
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/NiflheimDevs/dyslexics-clock/bootstrap"
-	"github.com/eclipse/paho.mqtt.golang"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func ConnectMosquitto(di *bootstrap.Di) mqtt.Client {
-	client := mqtt.NewClient(mqtt.NewClientOptions().SetClientID(di.Env.MQTT.ClientID).AddBroker(fmt.Sprintf("tcp://%s:%s", di.Env.MQTT.Address, di.Env.MQTT.Port)))
+	client := mqtt.NewClient(mqtt.NewClientOptions().
+		SetClientID(di.Env.MQTT.ClientID).
+		AddBroker(fmt.Sprintf("tcp://%s:%s", di.Env.MQTT.Address, di.Env.MQTT.Port)))
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
