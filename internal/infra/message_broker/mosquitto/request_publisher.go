a0444b546de69fd731b00af3b8657b235ff3b9b0
kiarash <kiarashs37@gmail.com>
2026-01-03 18:43:42 +0330

    minor change

--- internal/infra/message_broker/mosquitto/request_publisher.go
+++ internal/infra/message_broker/mosquitto/request_publisher.go
@@ -57,7 +57,7 @@ func (p *RequestPublisher) PublishColor(deviceID string, color string) error {
 func (p *RequestPublisher) PublishVolume(deviceID string, volume uint) error {
 	topic := fmt.Sprintf("devices/%s/volume", deviceID)
 
-	token := p.client.Publish(topic, p.qos, false, fmt.Sprint(volume))
+	token := p.client.Publish(topic, p.qos, false, []byte(fmt.Sprint(volume)))
 	token.Wait() // Wait for the publication to complete
 	if token.Error() != nil {
 		log.Printf("Error publishing volume for device %s to topic %s: %v", deviceID, topic, token.Error())
