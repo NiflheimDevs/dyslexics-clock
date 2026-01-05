740d4414424162e5598126ba9b9f022d1978e70e
kiarash <kiarashs37@gmail.com>
2026-01-06 02:41:56 +0330

    add autoreconnect to mqtt

--- internal/infra/message_broker/driver/driver.go
+++ internal/infra/message_broker/driver/driver.go
@@ -2,6 +2,7 @@ package driver
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/NiflheimDevs/dyslexics-clock/bootstrap"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -10,7 +11,14 @@ import (
 func ConnectMosquitto(di *bootstrap.Di) mqtt.Client {
 	client := mqtt.NewClient(mqtt.NewClientOptions().
 		SetClientID(di.Env.MQTT.ClientID).
-		AddBroker(fmt.Sprintf("tcp://%s:%s", di.Env.MQTT.Address, di.Env.MQTT.Port)))
+		AddBroker(fmt.Sprintf("tcp://%s:%s", di.Env.MQTT.Address, di.Env.MQTT.Port)).
+		SetAutoReconnect(true).
+		SetCleanSession(true).SetOnConnectHandler(func(c mqtt.Client) {
+		log.Println("MQTT connected")
+	}).
+		SetConnectionLostHandler(func(c mqtt.Client, err error) {
+			log.Printf("MQTT connection lost: %v", err)
+		}))
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
